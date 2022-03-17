f6370b7a36343ef9b70c6b286f0881fb6b34c4bf
kamillenikolajsen <knikolajsen4@gmail.com>
2022-03-17 10:24:10 +0100

    Add changes in login.html, .js and .css

--- js/login.js
+++ js/login.js
@@ -2,23 +2,28 @@ const out = function (str) {
   console.log(str);
 };
 
-document.addEventListener("DOMContentLoaded", creatLoginForm);
+document.addEventListener("DOMContentLoaded", createLoginForm);
 
 let loginForm;
 
 function createLoginForm() {
   loginForm = document.querySelector("#loginForm");
   loginForm.addEventListener("submit", handleLogin);
+  out(loginForm);
 }
 
 async function handleLogin(event) {
   event.preventDefault();
   //
   const form = event.currentTarget;
-  const url = event.action;
-
+  const url = form.action;
+  out(form);
+  out(url);
   try {
-    const formData = new FormData(form);
+    //const formData = new FormData(form);
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const formData = {email: email, password:password};
     const responseData = await postLogin(url, formData);
 
     out(responseData);
@@ -29,17 +34,19 @@ async function handleLogin(event) {
 
 async function postLogin(url, formData) {
   //HVAD ER DET??
-  const plainFormData = Object.fromEntries(formData.entries());
+  //const plainFormData = Object.fromEntries(formData.entries());
 
-  const jsonDataString = JSON.stringify(plainFormData);
-
+  const jsonDataString = JSON.stringify(formData);
+  out("this is the json string: " + jsonDataString)
   const fetchOptions = {
     method: "POST",
-    headers: { "Constent-Type": "application/json" },
+    headers: { 'Accept': 'application/json', "Content-Type": "application/json" },
     body: jsonDataString,
   };
 
   const response = await fetch(url, fetchOptions);
+
+  out(response);
 
   if (!response) {
     const errorMessage = await response.text();
@@ -49,12 +56,14 @@ async function postLogin(url, formData) {
   return response.json();
 }
 
-
-function validate(){
+/*
+async function validate(){
   //What we get from the user
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
+  const response = await fetch(email, password);
+  out(response);
   const dbEmail = null;
   const dbPassword = null;
 
@@ -63,7 +72,10 @@ function validate(){
     window.location = "index.html";
     return false;
   } else {
+    out("you didn't login")
   window.location = "login.html";
   return false;
   }
 }
+
+ */
