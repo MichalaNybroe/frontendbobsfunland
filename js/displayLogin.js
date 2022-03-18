8bcecfa42d23eaa17311bb1d16807ad994bca605
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-03-18 13:08:42 +0100

    login and header changes

--- js/displayLogin.js
+++ js/displayLogin.js
@@ -3,11 +3,16 @@ const loginHeader = document.querySelector("#loginHeader");
 displayHiddenHeaderTags();
 
 function displayHiddenHeaderTags() {
-  if (localStorage.getItem("login")) {
+  let retrievedObject = JSON.parse(localStorage.getItem('login'));
+  console.log(retrievedObject);
+  if (retrievedObject !== null) {
     document.querySelector("#bookingHeader").style.display = "inline-block";
     loginHeader.textContent = "Logout";
+    loginHeader.addEventListener("click", () => localStorage.removeItem("login"));
+    console.log("1");
   } else {
     document.querySelector("#bookingHeader").style.display = "none";
     loginHeader.textContent = "Login";
+    console.log("2");
   }
 }
