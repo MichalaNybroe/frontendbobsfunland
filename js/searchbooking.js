ea7ef23ddcb36fdaec32f86410332ccef1a411f9
Michala Nybroe <michalanim@gmail.com>
2022-03-23 10:36:13 +0100

    search bookings

--- js/searchbooking.js
+++ js/searchbooking.js
@@ -7,6 +7,8 @@ const searchSubmit = document.getElementById("searchButton");
 searchSubmit.addEventListener("click", sendSearch);
 
 async function sendSearch(event) {
+  emptyTable();
+
   event.preventDefault();
 
   const fetchOptions = {
@@ -20,6 +22,7 @@ async function sendSearch(event) {
   const bookings = await fetch(searchURL, fetchOptions).then((response) =>
     response.json()
   );
+  console.log(bookings)
 
   if (!bookings) {
     const errorMessage = await bookings.text();
@@ -31,49 +34,104 @@ async function sendSearch(event) {
   await bookings.forEach((booking) => {
     bookingSearchMap.set(booking.orderNumber, booking);
   });
-  bookingSearchMap.forEach((booking) => addRow(booking));
+
+  console.log(bookingSearchMap);
+
+  bookingSearchMap.forEach((booking) => addSearchRow(booking));
 }
 
 const searchBookingTable = document.getElementById("searchTableBookings");
-function addRow(booking) {
-  const rowCount = searchBookingTable.rows.length;
-  let columnCount = 0;
-  let row = searchBookingTable.insertRow(rowCount);
-  let cell = row.insertCell(columnCount++);
 
-  cell.innerText = booking.customer.name;
+console.log(searchBookingTable);
 
-  cell = row.insertCell(columnCount++);
-  cell.innerText = booking.customer.email;
+function addSearchRow(booking) {
+  const rowSearchCount = searchBookingTable.rows.length;
+  let columnSearchCount = 0;
 
-  cell = row.insertCell(columnCount++);
-  cell.innerText = booking.customer.phoneNumber;
+  let rowS = searchBookingTable.insertRow(rowSearchCount);
+  rowS.setAttribute("class", "searchTableRow");
+  let cellS = rowS.insertCell(columnSearchCount++);
+  cellS.innerText = booking.customer.name;
 
-  cell = row.insertCell(columnCount++);
-  cell.innerText = booking.orderNumber;
+  cellS = rowS.insertCell(columnSearchCount++);
+  cellS.innerText = booking.customer.email;
 
-  cell = row.insertCell(columnCount++);
-  cell.innerText = booking.date;
+  cellS = rowS.insertCell(columnSearchCount++);
+  cellS.innerText = booking.customer.phoneNumber;
 
-  cell = row.insertCell(columnCount++);
-  cell.innerText = booking.time;
+  cellS = rowS.insertCell(columnSearchCount++);
+  cellS.innerText = booking.orderNumber;
 
-  cell = row.insertCell(columnCount++);
-  cell.innerText = booking.activity.name;
+  cellS = rowS.insertCell(columnSearchCount++);
+  cellS.innerText = booking.date;
 
-  cell = row.insertCell(columnCount++);
-  cell.innerText = booking.nrOfParticipants;
+  cellS = rowS.insertCell(columnSearchCount++);
+  cellS.innerText = booking.time;
 
-  cell = row.insertCell(columnCount++);
-  cell.innerText = booking.instructor.email;
+  cellS = rowS.insertCell(columnSearchCount++);
+  cellS.innerText = booking.activity.name;
 
-  cell = row.insertCell(columnCount++);
+  cellS = rowS.insertCell(columnSearchCount++);
+  cellS.innerText = booking.nrOfParticipants;
+
+  cellS = rowS.insertCell(columnSearchCount++);
+  cellS.innerText = booking.instructor.email;
+
+  cellS = rowS.insertCell(columnSearchCount++);
   const deleteButton = document.createElement("input");
   deleteButton.type = "button";
   deleteButton.setAttribute("value", "Slet");
   deleteButton.setAttribute("class", "deleteButton");
   deleteButton.onclick = function () {
-    deleteBooking(booking, rowCount, row);
+    deleteSearchBooking(booking, rowSearchCount, rowS);
   };
-  cell.appendChild(deleteButton);
+  cellS.appendChild(deleteButton);
+
+  cellS = rowS.insertCell(columnSearchCount++);
+  const editButton = document.createElement("input");
+  editButton.type = "button";
+  editButton.setAttribute("value", "Rediger");
+  editButton.setAttribute("class", "editButton");
+  editButton.onclick = function () {
+    window.location.replace("editBooking.html");
+    updateBooking(booking, rowSearchCount, rowS);
+  };
+  cellS.appendChild(editButton);
+}
+
+function emptyTable() {
+  document.querySelectorAll('.searchTableRow').forEach(e => e.remove());
+  }
+
+async function deleteSearchBooking(booking, rowSearchCount, rowS) {
+  const response = await restSearchDeleteBooking(booking);
+  if (response) {
+    searchBookingTable.deleteRow(rowS.rowIndex);
+  } else {
+    console.log("Something went wrong in deleteBooking");
+  }
+}
+
+async function restSearchDeleteBooking(booking) {
+  const url = "http://localhost:8080/booking";
+
+  const formData = JSON.stringify(booking);
+
+  const fetchOptions = {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: "",
+  };
+
+  fetchOptions.body = formData;
+
+  const response = await fetch(url, fetchOptions);
+
+  if (!response) {
+    out("Something went wrong in restDeleteBooking");
+  }
+
+  return response;
 }
